c0de6c7e0011c80eb2050cc2246cc9ed92ca4b43
ESC <youhavethewrong@gmail.com>
2017-10-13 14:37:58 -0500

    Got the search to work.  Thanks venia!

--- project.clj
+++ project.clj
@@ -5,7 +5,8 @@
             :url "https://www.apache.org/licenses/LICENSE-2.0"}
   :dependencies [[org.clojure/clojure "1.9.0-beta1"]
                  [org.clojure/data.json "0.2.6"]
-                 [clj-http "3.0.1"]]
+                 [clj-http "3.0.1"]
+                 [vincit/venia "0.2.3"]]
   :main ^:skip-aot want-worn-wear.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/want_worn_wear/core.clj
+++ src/want_worn_wear/core.clj
@@ -1,13 +1,25 @@
 (ns want-worn-wear.core
   (:require [clojure.data.json :as json]
-            [clj-http.client :as client])
+            [clj-http.client :as client]
+            [clojure.java.io :as io]
+            [venia.core :as v])
   (:gen-class))
 
-(def base-url "https://reware-production.yerdle.io/v4/graphql")
+(def search-url "https://reware-production.yerdle.io/v4/graphql")
 (def useragent "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36")
 
+(def ww-query
+  (v/graphql-query
+   {:venia/queries [[:partner {:uuid "7d32ad83-330e-4ccc-ba03-3bb32ac113ac"}
+                     [[:categories {:slug "mens"}
+                       [:slug :title [:inventoryItemsForSale {:limit 50 :size "M"}
+                                      [:availableSizes :color :imageUrls :parentSKU
+                                       :price :title :priceRange]]]]]]]}))
+
+
 (def standard-params
-  {:client-params {"http.useragent" useragent}
+  {:body (json/write-str {:query ww-query})
+   :client-params {"http.useragent" useragent}
    :headers {"Accept-Language" "en-US,en;q=0.5"
              "Content-Type" "application/json"
              "Referer" "https://wornwear.patagonia.com/shop/mens"
@@ -19,7 +31,10 @@
   [url]
   (:body (client/post url standard-params)))
 
+(defn save-results
+  [f]
+  (spit f (post-search search-url)))
+
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
