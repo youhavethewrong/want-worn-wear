c219e67bf3c5ab73e81ebf61a0741ca878836801
ESC <youhavethewrong@gmail.com>
2017-10-13 15:37:55 -0500

    Format results into a nice report and build direct links to items.

--- src/want_worn_wear/core.clj
+++ src/want_worn_wear/core.clj
@@ -5,14 +5,19 @@
             [venia.core :as v])
   (:gen-class))
 
-(def search-url "https://reware-production.yerdle.io/v4/graphql")
 (def useragent "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36")
+
+(def search-url "https://reware-production.yerdle.io/v4/graphql")
+
+(defn build-item-url
+  [id color]
+  (format "https://wornwear.patagonia.com/shop/mens/%s/%s" id color))
 
 (def ww-query
   (v/graphql-query
    {:venia/queries [[:partner {:uuid "7d32ad83-330e-4ccc-ba03-3bb32ac113ac"}
                      [[:categories {:slug "mens"}
-                       [:slug :title [:inventoryItemsForSale {:limit 50 :size "M"}
+                       [:slug :title [:inventoryItemsForSale {:limit 100 :size "M"}
                                       [:availableSizes :color :imageUrls :parentSKU
                                        :price :title :priceRange]]]]]]]}))
 
@@ -33,8 +38,21 @@
 
 (defn save-results
   [f]
-  (spit f (post-search search-url)))
+  (let [body (post-search search-url)]
+    (spit f body)
+    body))
+
+(defn format-price
+  [price]
+  (format "$%4.2f" (/ price 100.0)))
+
+(defn display-results
+  [{:keys [color parentSKU price title]}]
+  (println (str title "\n" (format-price price) "\n" (build-item-url parentSKU color) "\n------------------------\n")))
 
 (defn -main
   [& args]
-  (println "Hello, World!"))
+  (let [data (json/read-str (save-results "out.json") :key-fn keyword)]
+    (dorun (map
+            #(display-results %)
+            (:inventoryItemsForSale (first (get-in data [:data :partner :categories])))))))
