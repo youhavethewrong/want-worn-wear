b534483268e926a6e5b6e7331ff60367e1135264
ESC <youhavethewrong@gmail.com>
2017-10-20 16:17:33 -0500

    Add deps in preparation for persisting and tracking items that show up.

--- project.clj
+++ project.clj
@@ -1,12 +1,13 @@
 (defproject want-worn-wear "0.1.0-SNAPSHOT"
   :description "Watch for interesting things to show up on worn wear."
-  :url "https://github.com/youhavethewrong/inth"
   :license {:name "Apache License, Version 2.0"
             :url "https://www.apache.org/licenses/LICENSE-2.0"}
   :dependencies [[org.clojure/clojure "1.9.0-beta1"]
+                 [org.clojure/java.jdbc "0.7.3"]
                  [org.clojure/data.json "0.2.6"]
                  [clj-http "3.0.1"]
-                 [vincit/venia "0.2.3"]]
+                 [vincit/venia "0.2.3"]
+                 [org.xerial/sqlite-jdbc "3.20.1"]]
   :main ^:skip-aot want-worn-wear.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
